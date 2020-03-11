3a96bbcfc90e838a5b53784bdac88456330e0e3c
“wjincheng” <2411465039@qq.com>
2020-03-11 14:45:27 +0800

    add test for lock error

--- src/test/java/cn/xpbootcamp/legacy_code/WalletTransactionTest.java
+++ src/test/java/cn/xpbootcamp/legacy_code/WalletTransactionTest.java
@@ -1,12 +1,19 @@
 package cn.xpbootcamp.legacy_code;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import cn.xpbootcamp.legacy_code.utils.RedisDistributedLock;
 import javax.transaction.InvalidTransactionException;
+import mockit.Expectations;
+import mockit.Mock;
 import mockit.Mocked;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class WalletTransactionTest {
 
+    @Mocked
+    RedisDistributedLock redisDistributedLock;
 
     @Test
     public void should_throw_except_when_buyerId_is_null() {
@@ -22,6 +29,27 @@ public class WalletTransactionTest {
         Assertions.assertThrows(InvalidTransactionException.class, ()->{
             walletTransaction.execute();
         });
+    }
+
+    @Test
+    public void should_return_false_when_lock_error() throws InvalidTransactionException{
+        String preAssignedId = "1";
+        Long buyerId = 1l;
+        Long sellerId = 1l;
+        Long productId = 1l;
+        String orderId = "1";
+        new Expectations() {
+            {
+                RedisDistributedLock.getSingletonInstance();
+                result = redisDistributedLock;
+
+                redisDistributedLock.lock(preAssignedId);
+                result = false;
+            }
+        };
+
+        WalletTransaction walletTransaction = new WalletTransaction(preAssignedId, buyerId, sellerId, productId, orderId);
+        assertFalse(walletTransaction.execute());
     }
 
 }
